2118b83058c44655409c2bbc604067e94834099d
Rian Santana <82774034+RianRBPS@users.noreply.github.com>
2026-03-23 15:12:33 -0300

    Implement user input for operations in client.py
    Added user input for operation and values, and updated message sending.

--- client.py
+++ client.py
@@ -1,9 +1,17 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *
+
+operacao = input("Operacao (add, subtract, multiply, divide): ")
+a = input("Valor 1: ")
+b = input("Valor 2: ")
+
+msg = f"{operacao},{a},{b}"
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
-s.close()               # close the connection
+s.connect((HOST, PORT))
+s.send(msg.encode())
+
+resp = s.recv(1024)
+print("Resposta:", resp.decode())
+
+s.close()
